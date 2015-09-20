adc7c61a069edbefb8153c7ce60cc75cbbf533bc
bautistaluis88 <bautista_luis_88@hotmail.com>
2015-09-19 21:23:46 -0300

    Archivo de configuracion

--- config/config.php
+++ config/config.php
@@ -2,17 +2,16 @@
 //Config -----
 $_config = array(
     //Host Configuration
-    'PROYECTO'=>'TucuCity',
-    'HOME'=>'http://localhost/tucucity/',
+    'PROYECTO'=>'FrameworkBase!',
+    'HOME'=>'http://localhost/frameworkbase/',
     //Database Configuration
-    'DB_HOST'=>'localhost',
-    'DB_USER'=>'root',
-    'DB_PASS'=>'root',
-    'DB_NAME'=>'MiDB',
+    'DB_HOST'=>'javiercani.net',
+    'DB_USER'=>'javierca_luis',
+    'DB_PASS'=>'31i7331i73',
+    'DB_NAME'=>'javierca_tucucity',
     'DB_PORT'=>3306,
     //Cantidad de / a leer en la URL
     'CONT'=>2
 );
 // -----------------------------
-
 ?>
