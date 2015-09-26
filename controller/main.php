b1f0e2adba39d4fdee47b6c714ec118b9f1710d5
javiercani <javier.caniparoli@gmail.com>
2015-09-26 19:35:18 -0300

    Moviendo las clases buscador a la carpeta controller y agregando modulo para RSS.

--- controller/main.php
+++ controller/main.php
@@ -2,8 +2,32 @@
 class Main{
 	public static function init()
     {
-        (new View())->show("main");
+        $rssnoticias = Main::getFeed("http://cdn02.ib.infobae.com/adjuntos/162/rss/Infobae.xml",3);//"http://noticias-tucuman.webnode.es/rss/novedades.xml");//"http://cdn02.am.infobae.com/adjuntos/163/rss/ahora.xml");
+        (new View())->show("main",array('rssnoticias'=>$rssnoticias));
     }
+
+    public static function getFeed($feed_url,$cantidad=5) {
+
+        $content = file_get_contents($feed_url);
+        $x = new SimpleXmlElement($content);
+
+        $rss = "<div class='col-sm-2'></div>";
+        $cnt = 0;
+        foreach($x->channel->item as $entry) {
+            if($cnt == 3 ) {
+                break;
+            }
+            $rss.= "<div class='col-sm-3'>";
+            $rss.= "<a href='$entry->link' title='$entry->title'>" . $entry->title . "</a><br>";
+            $rss.= "$entry->description";
+            $rss.= "</div>";
+            $cnt++;
+        }
+        $rss .= "<div class='col-sm-1'></div>";
+
+        return $rss;
+    }
+
 }
 
  ?>
